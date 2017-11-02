bb36868a12182c283311ee1507894ec0a50d62de
lcy <331957324@qq.com>
2017-11-02 17:34:29 +0800

    update log

---
+++ InterfaceTestFramework/Tool/Logger/logTool.py
@@ -0,0 +1,33 @@
+import logging
+from Tool.Config.configTool import ConfigTool
+
+
+class LogTool(object):
+
+    file_path = ConfigTool.get('Log', 'Path')
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=file_path,
+                        filemode='w')
+
+    def debug(self, message):
+        logging.debug(message)
+
+    def info(self, message):
+        logging.info(message)
+
+    def warning(self, message):
+        logging.warning(message)
+
+    def error(self, message):
+        logging.error(message)
+
+    def critical(self, message):
+        logging.critical(message)
+
+
+
+
+
+
